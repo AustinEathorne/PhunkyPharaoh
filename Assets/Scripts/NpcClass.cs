4e15511c97991abff9f6f0d378c091b02c1e1d9f
Austin Eathorne <austineathorne@gmail.com>
2017-10-24 17:47:11 -0400

    NPC class & generator outlined, added skeleton for GameManager and LevelManager

---
+++ Assets/Scripts/NpcClass.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcClass : MonoBehaviour 
+{
+	private string m_name = "";
+	private Sprite m_sprite = null;
+	private List<string> m_dialogueLines = new List<string>();
+	private Dictionary<int, List<string>> m_dialogueOptions = new Dictionary<int, List<string>>();
+
+	// Initialized by NpcGenerator
+	public void InitializeNpc(string name, Sprite sprite, List<string> dialogueLines, Dictionary<int, List<string>> dialogueOptions)
+	{
+		this.m_name = name;
+		this.m_sprite = sprite;
+		this.m_dialogueLines = dialogueLines;
+		this.m_dialogueOptions = dialogueOptions;
+	}
+
+	// Return the desired dialogue line
+	private string GetDialogue(int lineIndex)
+	{
+		return this.m_dialogueLines[lineIndex];
+	}
+
+	// Return the desired list of possible responses
+	private List<string> GetDialogueResponses(int dialogueIndex)
+	{
+		return this.m_dialogueOptions[dialogueIndex];
+	}
+}
---
+++ Assets/Scripts/NpcGenerator.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Holds the list of possible sprites + dialogue options & their corresponding responses
+
+public class NpcGenerator : MonoBehaviour 
+{
+	private bool isInitialized = false;
+
+	// Create new list & dictionary for new dialogue, each list holds a set of lines for the npc to say, dictionary responses (List<string>) corresponds to the lineIndex for the dialogue list
+	private List<string> dialogue1 = new List<string>();
+	private Dictionary<int, List<string>> dialogueResponses1 = new Dictionary<int, List<string>>();
+
+
+	private IEnumerator Start()
+	{
+		List<string> responses = new List<string>();
+		// Add new dialogue here
+
+		//Dialogue1
+		//Line0
+		dialogue1.Add ("Hey, how's it going?"); // dialogue
+		responses = new List<string>(); //responses
+		responses.Add ("Great");
+		responses.Add ("Alright");
+		responses.Add ("Bad");
+		dialogueResponses1.Add (0, responses); //dicitionary for responses
+		//Line1
+		dialogue1.Add ("What's your favourite colour?");
+		responses = new List<string>();
+		responses.Add ("Red");
+		responses.Add ("Green");
+		responses.Add ("Blue");
+		dialogueResponses1.Add (1, responses);
+		//Line2
+		dialogue1.Add ("You have nice hair!");
+		responses = new List<string>();
+		responses.Add ("Nah b");
+		responses.Add ("Thanks!");
+		responses.Add ("Nah, you have nice hair.");
+		dialogueResponses1.Add (2, responses);
+
+		//Dialogue2
+
+
+		this.isInitialized = true;
+		yield return null;
+	}
+
+	public bool GetIsInitiliazed()
+	{
+		return this.isInitialized;
+	}
+}
